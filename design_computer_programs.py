3832e0ab8f9f9138e5ec728cb27bde32864a3772
Ashley Felton <ashley@ropable.com>
2012-04-29 14:16:24 +0800

    Added a couple of my solutions.

--- design_computer_programs.py
+++ design_computer_programs.py
@@ -1,28 +1,39 @@
+#!/usr/bin/env python
 '''
 CS212 Design of Computer Programs
 '''
 def poker(hand):
-    '''
-    Return the highest-ranked hand of a list of poker hands.
-    '''
-    return None
+    "Return a list of winning hands: poker([hand,...]) => [hand,...]"
+    return max(hands, key=hand_rank)
 
 def straight(ranks):
     "Return True if the ordered ranks form a 5-card straight."
+    # My solution:
     ranks.sort() # Sort lowest to highest.
-    for i in xrange(4): # Assume a hand is 5 cards only.
+    for i in range(4): # Assume a hand is 5 cards only.
         if ranks[i] != (ranks[i+1]-1):
             return False
     return True
+    # Instructor solution:
+    #return (max(ranks)-min(ranks) == 4) and len(set(ranks)) == 5
 
 def flush(hand):
     "Return True if all the cards have the same suit."
-    #hand = ['6C','7C','8C','9C','TC']
-    for i in xrange(4):
+    # My solution:
+    for i in range(4):
         if hand[i][-1] != hand[i+1][-1]:
             return False
     return True
+    # Instructor solution:
+    #suits = [s for r,s in hand]
+    #return len(set(suits)) == 1
 
+def card_ranks(hand):
+    "Return a list of the ranks, sorted with higher first."
+    ranks = ['--23456789TJQKA'.index(r) for r, s in hand]
+    ranks.sort(reverse = True)
+    return ranks
+    
 def test():
     "Test cases for the functions in poker program."
     sf = "6C 7C 8C 9C TC".split()
