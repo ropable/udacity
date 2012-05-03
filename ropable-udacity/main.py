84e3f825f5337fde60a1126ed60c98de192632d4
Ashley Felton <ashley@ropable.com>
2012-05-03 15:53:24 +0800

    small bit of work

--- ropable-udacity/main.py
+++ ropable-udacity/main.py
@@ -2,12 +2,31 @@
 import webapp2
 
 form = '''
-<form method="post">
-    What is your birthday?
-    <input name="q">
-    <input type="submit">
-</form>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Unit 2 Rot 13</title>
+  </head>
+  <body>
+    <h2>Enter some text to ROT13:</h2>
+    <form method="post">
+      <textarea name="text"></textarea>
+      </br>
+      <input type="submit">
+    </form>
+  </body>
+</html>
 '''
+
+import cgi, string
+
+def rot13_html(text=None):
+    if not text: return None
+    rot13 = string.maketrans("ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz",
+        "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm")
+    text = string.translate(text, rot13)
+    return cgi.escape(text)
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         #self.response.headers['Content-Type'] = 'text/plain'
@@ -19,6 +38,6 @@ class TestHandler(webapp2.RequestHandler):
         self.response.out.write(self.request)
         #q = self.request.get('q')
         #self.response.out.write(q)
-        
+
 app = webapp2.WSGIApplication([('/', MainHandler),('/testform', TestHandler)],
     debug=True)
