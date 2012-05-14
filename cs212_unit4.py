17a329474f4e1b1fe7cfda1a56c813d661ac8d9c
Ashley Felton <ashley@ropable.com>
2012-05-14 13:17:48 +0800

    WIP hw4-1

--- cs212_unit4.py
+++ cs212_unit4.py
@@ -1,14 +1,14 @@
 # HW 4-1
 # -----------------
 # User Instructions
-# 
+#
 # In this problem you will be refactoring the bsuccessors function.
 # Your new function, bsuccessors3, will take a state as an input
-# and return a dict of {state:action} pairs. 
+# and return a dict of {state:action} pairs.
 #
-# A state is a (here, there, light) tuple. Here and there are 
+# A state is a (here, there, light) tuple. Here and there are
 # frozensets of people (each person is represented by an integer
-# which corresponds to their travel time), and light is 0 if 
+# which corresponds to their travel time), and light is 0 if
 # it is on the `here` side and 1 if it is on the `there` side.
 #
 # An action is a tuple of (travelers, arrow), where the arrow is
@@ -16,7 +16,7 @@
 # of what your function's input and output should look like.
 def bsuccessors3(state):
     '''Return a dict of {state:action} pairs.  State is (here, there, light)
-    where here and there are frozen sets of people, light is 0 if the light is 
+    where here and there are frozen sets of people, light is 0 if the light is
     on the here side and 1 if it is on the there side.
     Action is a tuple (travelers, arrow) where arrow is '->' or '<-'
     '''
@@ -31,7 +31,7 @@ def bsuccessors3(state):
         for s in sets:
             d[(here | s, there - s, 0)] = (s, '<-')
     return d
-    
+
 def bsuccessors2(state):
     '''Return a dict of {state:action} pairs.  A state is a (here, there) tuple,
     where here and there are frozensets of people (indicated by their times) and/or
@@ -54,13 +54,13 @@ def test_bsuccessors2():
     assert bsuccessors3((frozenset([1]), frozenset([]), 0)) == {(frozenset([]), frozenset([1]), 1)  :  (set([1]), '->')}
 
     assert bsuccessors3((frozenset([1, 2]), frozenset([]), 0)) == {
-            (frozenset([1]), frozenset([2]), 1)    :  (set([2]), '->'), 
-            (frozenset([]), frozenset([1, 2]), 1)  :  (set([1, 2]), '->'), 
+            (frozenset([1]), frozenset([2]), 1)    :  (set([2]), '->'),
+            (frozenset([]), frozenset([1, 2]), 1)  :  (set([1, 2]), '->'),
             (frozenset([2]), frozenset([1]), 1)    :  (set([1]), '->')}
 
     assert bsuccessors3((frozenset([2, 4]), frozenset([3, 5]), 1)) == {
-            (frozenset([2, 4, 5]), frozenset([3]), 0)   :  (set([5]), '<-'), 
-            (frozenset([2, 3, 4, 5]), frozenset([]), 0) :  (set([3, 5]), '<-'), 
+            (frozenset([2, 4, 5]), frozenset([3]), 0)   :  (set([5]), '<-'),
+            (frozenset([2, 3, 4, 5]), frozenset([]), 0) :  (set([3, 5]), '<-'),
             (frozenset([2, 3, 4]), frozenset([5]), 0)   :  (set([3]), '<-')}
     return 'test_bsuccessors2 pass'
 
@@ -68,22 +68,22 @@ print test_bsuccessors2()
 # HW 4-2
 # -----------------
 # User Instructions
-# 
+#
 # In this problem, you will solve the pouring problem for an arbitrary
-# number of glasses. Write a function, more_pour_problem, that takes 
-# as input capacities, goal, and (optionally) start. This function should 
+# number of glasses. Write a function, more_pour_problem, that takes
+# as input capacities, goal, and (optionally) start. This function should
 # return a path of states and actions.
 #
-# Capacities is a tuple of numbers, where each number represents the 
-# volume of a glass. 
+# Capacities is a tuple of numbers, where each number represents the
+# volume of a glass.
 #
 # Goal is the desired volume and start is a tuple of the starting levels
 # in each glass. Start defaults to None (all glasses empty).
 #
 # The returned path should look like [state, action, state, action, ... ]
-# where state is a tuple of volumes and action is one of ('fill', i), 
-# ('empty', i), ('pour', i, j) where i and j are indices indicating the 
-# glass number. 
+# where state is a tuple of volumes and action is one of ('fill', i),
+# ('empty', i), ('pour', i, j) where i and j are indices indicating the
+# glass number.
 def more_pour_problem(capacities, goal, start=None):
     '''The first argument is a tuple of capacities (numbers) of glasses; the
     goal is a number which we must achieve in some glass.  start is a tuple
@@ -100,10 +100,10 @@ def more_pour_problem(capacities, goal, start=None):
     return shortest_path_search(start, pour_successors, capacities, pour_problem_goal, goal)
 
     # OUT: [(0, 0, 0, 0), ('fill', 2), (0, 0, 4, 0)]
-    
+
 def pour_successors(state, capacities):
     '''Return a dict of {state:action} pairs.  State is (here, there, light)
-    where here and there are frozen sets of people, light is 0 if the light is 
+    where here and there are frozen sets of people, light is 0 if the light is
     on the here side and 1 if it is on the there side.
     Action is a tuple (travelers, arrow) where arrow is '->' or '<-'
     '''
@@ -140,7 +140,7 @@ def pour_successors(state, capacities):
                         suc[i2] += transfer
                         successors[tuple(suc)] = ('pour', i, i2)
     return successors
-    
+
 def pour_problem_goal(state, goal):
     '''Does the tuple contain the desired volume?
     '''
@@ -149,7 +149,7 @@ def pour_problem_goal(state, goal):
     else:
         return False
 
-Fail = [] 
+Fail = []
 def shortest_path_search(start, successors, capacities, is_goal, goal):
     '''Find the shortest path from start state to a state
     such that is_goal(state) is true.'''
@@ -174,7 +174,7 @@ def test_more_pour():
     assert more_pour_problem((1, 2, 4, 8), 4) == [
         (0, 0, 0, 0), ('fill', 2), (0, 0, 4, 0)]
     assert more_pour_problem((1, 2, 4), 3) == [
-        (0, 0, 0), ('fill', 2), (0, 0, 4), ('pour', 2, 0), (1, 0, 3)] 
+        (0, 0, 0), ('fill', 2), (0, 0, 4), ('pour', 2, 0), (1, 0, 3)]
     starbucks = (8, 12, 16, 20, 24)
     assert not any(more_pour_problem(starbucks, odd) for odd in (3, 5, 7, 9))
     assert all(more_pour_problem((1, 3, 9, 27), n) for n in range(28))
@@ -182,3 +182,128 @@ def test_more_pour():
     return 'test_more_pour passes'
 
 print test_more_pour()
+
+# HW 4-3
+# -----------------
+# User Instructions
+#
+# Write a function, subway, that takes lines as input (read more about
+# the **lines notation in the instructor comments box below) and returns
+# a dictionary of the form {station:{neighbor:line, ...}, ... }
+#
+# For example, when calling subway(boston), one of the entries in the
+# resulting dictionary should be 'foresthills': {'backbay': 'orange'}.
+# This means that foresthills only has one neighbor ('backbay') and
+# that neighbor is on the orange line. Other stations have more neighbors:
+# 'state', for example, has 4 neighbors.
+#
+# Once you've defined your subway function, you can define a ride and
+# longest_ride function. ride(here, there, system) takes as input
+# a starting station (here), a destination station (there), and a subway
+# system and returns the shortest path.
+#
+# longest_ride(system) returns the longest possible ride in a given
+# subway system.
+
+# -------------
+# Grading Notes
+#
+# The subway() function will not be tested directly, only ride() and
+# longest_ride() will be explicitly tested. If your code passes the
+# assert statements in test_ride(), it should be marked correct.
+
+def subway(**lines):
+    """Define a subway map. Input is subway(linename='station1 station2...'...).
+    Convert that and return a dict of the form: {station:{neighbor:line,...},...}"""
+    d = {}
+    # {'foresthills':{'backbay':'orange'}, 'state':{'aquarium':'blue','government':'blue','haymarket':'orange','downtown':'orange'} }
+    for k,v in lines.items(): #k=line name, v=stations
+        stations = v.split() # List of stations ['one, 'two', 'three']
+        line_length = len(stations) - 1
+        for i, s in enumerate(stations): #i=station index, s=station name
+            # Assumption: lines have at least two stations (i.e. a station has at least one neighbour).
+            if d.has_key(s):
+                # The station already exists; add neighbours on the new line.
+                if i == 0: # First station (one neighbour)
+                    t = d[s] # Copy existing
+                    d[s] = {(stations[i+1]:k)}
+                elif i == line_length: # Last station
+                    d[s].update(stations[i-1]:k)
+                else:
+                    d[s].update(stations[i-1]:k)
+                    d[s].update(stations[i+1]:k)
+            else:
+                d[s] = {}
+                if i == 0: # First station (one neighbour)
+                    d[s] = {stations[i+1]:k}
+                elif i == line_length: # Last station
+                    d[s] = {stations[i-1]:k}
+                else: # middle station (two neighbours)
+                    d[s] = {stations[i-1]:k, stations[i+1]:k}
+    return d
+
+boston = subway(
+    blue='bowdoin government state aquarium maverick airport suffolk revere wonderland',
+    orange='oakgrove sullivan haymarket state downtown chinatown tufts backbay foresthills',
+    green='lechmere science north haymarket government park copley kenmore newton riverside',
+    red='alewife davis porter harvard central mit charles park downtown south umass mattapan')
+
+def ride(here, there, system=boston):
+    "Return a path on the subway system from here to there."
+    ## your code here
+    pass
+
+def longest_ride(system):
+    """"Return the longest possible 'shortest path'
+    ride between any two stops in the system."""
+    ## your code here
+    pass
+
+def shortest_path_search(start, successors, is_goal):
+    """Find the shortest path from start state to a state
+    such that is_goal(state) is true."""
+    if is_goal(start):
+        return [start]
+    explored = set() # set of states we have visited
+    frontier = [ [start] ] # ordered list of paths we have blazed
+    while frontier:
+        path = frontier.pop(0)
+        s = path[-1]
+        for (state, action) in successors(s).items():
+            if state not in explored:
+                explored.add(state)
+                path2 = path + [action, state]
+                if is_goal(state):
+                    return path2
+                else:
+                    frontier.append(path2)
+    return []
+
+def path_states(path):
+    "Return a list of states in this path."
+    return path[0::2]
+
+def path_actions(path):
+    "Return a list of actions in this path."
+    return path[1::2]
+'''
+def test_ride():
+    assert ride('mit', 'government') == [
+        'mit', 'red', 'charles', 'red', 'park', 'green', 'government']
+    assert ride('mattapan', 'foresthills') == [
+        'mattapan', 'red', 'umass', 'red', 'south', 'red', 'downtown',
+        'orange', 'chinatown', 'orange', 'tufts', 'orange', 'backbay', 'orange', 'foresthills']
+    assert ride('newton', 'alewife') == [
+        'newton', 'green', 'kenmore', 'green', 'copley', 'green', 'park', 'red', 'charles', 'red',
+        'mit', 'red', 'central', 'red', 'harvard', 'red', 'porter', 'red', 'davis', 'red', 'alewife']
+    assert (path_states(longest_ride(boston)) == [
+        'wonderland', 'revere', 'suffolk', 'airport', 'maverick', 'aquarium', 'state', 'downtown', 'park',
+        'charles', 'mit', 'central', 'harvard', 'porter', 'davis', 'alewife'] or
+        path_states(longest_ride(boston)) == [
+                'alewife', 'davis', 'porter', 'harvard', 'central', 'mit', 'charles',
+                'park', 'downtown', 'state', 'aquarium', 'maverick', 'airport', 'suffolk', 'revere', 'wonderland'])
+    assert len(path_states(longest_ride(boston))) == 16
+    return 'test_ride passes'
+
+print test_ride()
+'''
