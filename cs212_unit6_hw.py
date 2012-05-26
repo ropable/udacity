8faa2661bf4cf80d8d7284193e81424bf4ab9f7b
Ashley Felton <ashley@ropable.com>
2012-05-26 19:25:24 +0800

    CS212 HW6-1 completed.

---
+++ cs212_unit6_hw.py
@@ -0,0 +1,101 @@
+# -----------------
+# User Instructions
+# 
+# This homework deals with anagrams. An anagram is a rearrangement 
+# of the letters in a word to form one or more new words. 
+#
+# Your job is to write a function anagrams(), which takes as input 
+# a phrase and an optional argument, shortest, which is an integer 
+# that specifies the shortest acceptable word. Your function should
+# return a set of all the possible combinations of anagrams. 
+#
+# Your function should not return every permutation of a multi word
+# anagram: only the permutation where the words are in alphabetical
+# order. For example, for the input string 'ANAGRAMS' the set that 
+# your function returns should include 'AN ARM SAG', but should NOT 
+# include 'ARM SAG AN', or 'SAG AN ARM', etc...
+
+def anagrams(phrase, shortest=2):
+    '''Return a set of phrases with words from WORDS that form anagram
+    of phrase. Spaces can be anywhere in phrase or anagram. All words 
+    have length >= shortest. Phrases in answer must have words in 
+    lexicographic order (not all permutations).'''
+    anagrams = set()
+    frontier = []
+    phrase = phrase.replace(' ', '') # Replace spaces.
+    # Get our frontier started.
+    for t in extend_anagram(phrase, shortest):
+        frontier.append( (t[0], [t[1]]) ) # Tuple: ([], remaining phrase)
+    while frontier:
+        path = frontier.pop(0)
+        if not path[0]: # No remaining letters in the phrase.
+            anagrams.add(' '.join(sorted(path[1]))) # Add the to set of discovered anagrams.
+        else:
+            for t in extend_anagram(path[0], shortest):
+                path2 = (t[0], [t[1]] + path[1])
+                frontier.append(path2)
+    return anagrams
+
+def extend_anagram(phrase, shortest):
+    '''
+    Pass in a word, return a list of tuples: [(remainder, word1), (remainder, word2),...]
+    '''
+    result = []
+    words = sorted([w for w in find_words(phrase) if len(w) >= shortest])
+    for word in words:
+        result.append((removed(phrase, word), word))
+    return result
+    
+# ------------
+# Helpful functions
+# 
+# You may find the following functions useful. These functions
+# are identical to those we defined in lecture. 
+
+def removed(letters, remove):
+    "Return a str of letters, but with each letter in remove removed once."
+    for L in remove:
+        letters = letters.replace(L, '', 1)
+    return letters
+
+def find_words(letters):
+    return extend_prefix('', letters, set())
+
+def extend_prefix(pre, letters, results):
+    if pre in WORDS: results.add(pre)
+    if pre in PREFIXES:
+        for L in letters:
+            extend_prefix(pre+L, letters.replace(L, '', 1), results)
+    return results
+
+def prefixes(word):
+    "A list of the initial sequences of a word, not including the complete word."
+    return [word[:i] for i in range(len(word))]
+
+def readwordlist(filename):
+    "Return a pair of sets: all the words in a file, and all the prefixes. (Uppercased.)"
+    wordset = set(open(filename).read().upper().split())
+    prefixset = set(p for word in wordset for p in prefixes(word))
+    return wordset, prefixset
+
+WORDS, PREFIXES = readwordlist('words4k.txt')
+
+# ------------
+# Testing
+# 
+# Run the function test() to see if your function behaves as expected.
+
+def test_anagrams():
+    assert 'DOCTOR WHO' in anagrams('TORCHWOOD')
+    assert 'BOOK SEC TRY' in anagrams('OCTOBER SKY')
+    assert 'SEE THEY' in anagrams('THE EYES')
+    assert 'LIVES' in anagrams('ELVIS')
+    assert anagrams('PYTHONIC') == set([
+        'NTH PIC YO', 'NTH OY PIC', 'ON PIC THY', 'NO PIC THY', 'COY IN PHT',
+        'ICY NO PHT', 'ICY ON PHT', 'ICY NTH OP', 'COP IN THY', 'HYP ON TIC',
+        'CON PI THY', 'HYP NO TIC', 'COY NTH PI', 'CON HYP IT', 'COT HYP IN',
+        'CON HYP TI'])
+    return 'tests pass'
+
+print test_anagrams()
+
