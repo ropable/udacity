0b6cfeb86a197358da389558b2a18e04e27f5559
Ashley Felton <ashley@ropable.com>
2013-12-19 22:07:42 +0800

    Exam 4 4/5 answers correct.

--- cs259/exam4.py
+++ cs259/exam4.py
@@ -47,6 +47,45 @@ def remove_html_markup(s):
     return out
 
 
+def f1(ml):
+    if len(ml) < 6:
+        return -1
+    elif len(ml) > 12:
+        return 1
+    else:
+        return 0
+
+
+def f2(ms):
+    digits = 0
+    letters = 0
+    for c in ms:
+        if c in "1234567890":
+            digits += 1
+        elif c.isalpha():
+            letters += 1
+    other = len(ms) - digits - letters
+    grade = 0
+
+    if (other + digits) > 3:
+        grade += 1
+    elif other < 1:
+        grade -= 1
+
+    return grade
+
+
+def f3(mn):
+    forbidden = ["pass", "123", "qwe", "111"]
+    grade = 0
+    for word in forbidden:
+        if mn.find(word) > -1:
+            grade -= 1
+    if mn.find("%") > -1:
+        grade += 1
+    return grade
+
+
 # global variable to keep the coverage data in
 coverage = {}
 return_arg = None
@@ -84,7 +123,8 @@ def print_tables(tables):
     # Print out values of phi, and result of runs for each covered line
     for filename in tables.keys():
         lines = open(filename).readlines()
-        for i in range(30, 47):  # lines of the remove_html_markup in this file
+        #for i in range(30, 47):  # lines of the remove_html_markup in this file
+        for i in range(59, 75):  # lines of the f2 in this file
             if (i + 1) in tables[filename]:
                 (n11, n10, n01, n00) = tables[filename][i + 1]
                 try:
@@ -169,11 +209,8 @@ inputs_line = [
     '""',
     "<p>"]
 runs = run_tests(inputs_line)
-
 tables = init_tables(runs)
-
 tables = compute_n(tables)
-
 print_tables(tables)
 
 # These are the input values you should test the mystery function with
@@ -213,13 +250,13 @@ def mystery(magic):
     print magic, r1, r2, r3
 
     if r1 < 0 or r3 < 0:
-        return "FAIL"
+        i = "FAIL"
     elif (r1 + r2 + r3) < 0:
-        return "FAIL"
+        i = "FAIL"
     elif r1 == 0 and r2 == 0:
-        return "FAIL"
+        i = "FAIL"
     else:
-        return "PASS"
+        i = "PASS"
 
     for t in tmp:
         t.append(i)
@@ -227,45 +264,6 @@ def mystery(magic):
     # Now we have a list of tuples: (function, return, PASS/FAIL)
 
     return i
-
-
-def f1(ml):
-    if len(ml) < 6:
-        return -1
-    elif len(ml) > 12:
-        return 1
-    else:
-        return 0
-
-
-def f2(ms):
-    digits = 0
-    letters = 0
-    for c in ms:
-        if c in "1234567890":
-            digits += 1
-        elif c.isalpha():
-            letters += 1
-    other = len(ms) - digits - letters
-    grade = 0
-
-    if (other + digits) > 3:
-        grade += 1
-    elif other < 1:
-        grade -= 1
-
-    return grade
-
-
-def f3(mn):
-    forbidden = ["pass", "123", "qwe", "111"]
-    grade = 0
-    for word in forbidden:
-        if mn.find(word) > -1:
-            grade -= 1
-    if mn.find("%") > -1:
-        grade += 1
-    return grade
 
 
 # Run the mystery function with the inputs
@@ -276,19 +274,19 @@ for i in inputs:
 #{'n11': 0, 'n10': 0, 'n01': 0, 'n00': 0}
 d = {
     'f1': {
-        'pos': [0, 0, 0, 0],
-        'zero': [0, 0, 0, 0],
-        'neg': [0, 0, 0, 0]
+        '1': [0, 0, 0, 0],
+        '0': [0, 0, 0, 0],
+        '-1': [0, 0, 0, 0]
     },
     'f2': {
-        'pos': [0, 0, 0, 0],
-        'zero': [0, 0, 0, 0],
-        'neg': [0, 0, 0, 0]
+        '1': [0, 0, 0, 0],
+        '0': [0, 0, 0, 0],
+        '-1': [0, 0, 0, 0]
     },
     'f3': {
-        'pos': [0, 0, 0, 0],
-        'zero': [0, 0, 0, 0],
-        'neg': [0, 0, 0, 0]
+        '1': [0, 0, 0, 0],
+        '0': [0, 0, 0, 0],
+        '-1': [0, 0, 0, 0]
     },
 }
 
@@ -296,99 +294,99 @@ for i in returns:
     if i[0] == 'f1':  # f1
         if i[1] > 0:  # Positive
             if i[2] == 'PASS':
-                d['f1']['pos'][0] += 1
+                d['f1']['1'][0] += 1
             else:
-                d['f1']['pos'][1] += 1
+                d['f1']['1'][1] += 1
         else:  # Zero or negative.
             if i[2] == 'PASS':
-                d['f1']['pos'][2] += 1
+                d['f1']['1'][2] += 1
             else:
-                d['f1']['pos'][3] += 1
+                d['f1']['1'][3] += 1
         if i[1] == 0:  # Zero
             if i[2] == 'PASS':
-                d['f1']['zero'][0] += 1
+                d['f1']['0'][0] += 1
             else:
-                d['f1']['zero'][1] += 1
+                d['f1']['0'][1] += 1
         else:  # Positive or negative.
             if i[2] == 'PASS':
-                d['f1']['zero'][2] += 1
+                d['f1']['0'][2] += 1
             else:
-                d['f1']['zero'][3] += 1
+                d['f1']['0'][3] += 1
         if i[1] < 0:  # negative
             if i[2] == 'PASS':
-                d['f1']['neg'][0] += 1
+                d['f1']['-1'][0] += 1
             else:
-                d['f1']['neg'][1] += 1
+                d['f1']['-1'][1] += 1
         else:  # Positive or negative.
             if i[2] == 'PASS':
-                d['f1']['neg'][2] += 1
+                d['f1']['-1'][2] += 1
             else:
-                d['f1']['neg'][3] += 1
+                d['f1']['-1'][3] += 1
     if i[0] == 'f2':  # f2
         if i[1] > 0:  # Positive
             if i[2] == 'PASS':
-                d['f2']['pos'][0] += 1
+                d['f2']['1'][0] += 1
             else:
-                d['f2']['pos'][1] += 1
+                d['f2']['1'][1] += 1
         else:  # Zero or negative.
             if i[2] == 'PASS':
-                d['f2']['pos'][2] += 1
+                d['f2']['1'][2] += 1
             else:
-                d['f2']['pos'][3] += 1
+                d['f2']['1'][3] += 1
         if i[1] == 0:  # Zero
             if i[2] == 'PASS':
-                d['f2']['zero'][0] += 1
+                d['f2']['0'][0] += 1
             else:
-                d['f2']['zero'][1] += 1
+                d['f2']['0'][1] += 1
         else:  # Positive or negative.
             if i[2] == 'PASS':
-                d['f2']['zero'][2] += 1
+                d['f2']['0'][2] += 1
             else:
-                d['f2']['zero'][3] += 1
+                d['f2']['0'][3] += 1
         if i[1] < 0:  # negative
             if i[2] == 'PASS':
-                d['f2']['neg'][0] += 1
+                d['f2']['-1'][0] += 1
             else:
-                d['f2']['neg'][1] += 1
+                d['f2']['-1'][1] += 1
         else:  # Positive or negative.
             if i[2] == 'PASS':
-                d['f2']['neg'][2] += 1
+                d['f2']['-1'][2] += 1
             else:
-                d['f2']['neg'][3] += 1
+                d['f2']['-1'][3] += 1
     if i[0] == 'f3':  # f3
         if i[1] > 0:  # Positive
             if i[2] == 'PASS':
-                d['f3']['pos'][0] += 1
+                d['f3']['1'][0] += 1
             else:
-                d['f3']['pos'][1] += 1
+                d['f3']['1'][1] += 1
         else:  # Zero or negative.
             if i[2] == 'PASS':
-                d['f3']['pos'][2] += 1
+                d['f3']['1'][2] += 1
             else:
-                d['f3']['pos'][3] += 1
+                d['f3']['1'][3] += 1
         if i[1] == 0:  # Zero
             if i[2] == 'PASS':
-                d['f3']['zero'][0] += 1
+                d['f3']['0'][0] += 1
             else:
-                d['f3']['zero'][1] += 1
+                d['f3']['0'][1] += 1
         else:  # Positive or negative.
             if i[2] == 'PASS':
-                d['f3']['zero'][2] += 1
+                d['f3']['0'][2] += 1
             else:
-                d['f3']['zero'][3] += 1
+                d['f3']['0'][3] += 1
         if i[1] < 0:  # negative
             if i[2] == 'PASS':
-                d['f3']['neg'][0] += 1
+                d['f3']['-1'][0] += 1
             else:
-                d['f3']['neg'][1] += 1
+                d['f3']['-1'][1] += 1
         else:  # Positive or negative.
             if i[2] == 'PASS':
-                d['f3']['neg'][2] += 1
+                d['f3']['-1'][2] += 1
             else:
-                d['f3']['neg'][3] += 1
+                d['f3']['-1'][3] += 1
 
-print d
-answer_value = 0   # precision to 4 decimal places.
+
+answer_function_phi = 0.0
 
 for func, tables in d.iteritems():
     for group, values in tables.iteritems():
@@ -396,14 +394,22 @@ for func, tables in d.iteritems():
             phi_val = round(phi(*values), 4)
         except:
             phi_val = None
-        #print('{0}: {1} phi={2}'.format(func, group, phi_val))
+        print('{0}: {1} phi={2}'.format(func, group, phi_val))
 
-        if phi_val and phi_val > answer_value:
-            answer_value = phi_val
+        if phi_val and phi_val > answer_function_phi:
+            answer_function_phi = phi_val
             answer_function = func
             answer_bin = group
+
+answer_line_phi = round(answer_function_phi / answer_function_phi, 4)  # precision to 4 decimal places.
 
 # Final answer:
 print answer_function
 print answer_bin
-print answer_value
+print answer_function_phi
+print answer_line_phi
+#answer_function = "f2"   # One of f1, f2, f3
+#answer_bin = 1          # One of 1, 0, -1
+#answer_function_phi = 0.6547    # precision to 4 decimal places.
+#answer_line_phi = 1.0000  # precision to 4 decimal places.
+answer_line = ["if False:", 'return "FAIL"']  # lines of code
